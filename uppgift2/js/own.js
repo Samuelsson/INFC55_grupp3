87375002c265f96daac61daa43edb9842677d6a7
Eric Nordlund <eric@epitet.net>
2014-11-26 14:55:17 +0100

    Added JS-login to flighttrips.html

--- uppgift2/js/own.js
+++ uppgift2/js/own.js
@@ -2,7 +2,8 @@ function login()
 {
 	//Create a prompt to get the user name
 	var login_name = prompt("Enter login name");
-	if (login_name != null) 
+	if (login_name.length > 0) 
 		{
-			document.getElementById("login_box").innerHTML = "User: " + login_name; //If a name have been entered, send it to #loginbox
+			document.getElementById("login_box").innerHTML = "Logged in User: " + login_name; //If a name have been entered, send it to #loginbox
 		}
+}
