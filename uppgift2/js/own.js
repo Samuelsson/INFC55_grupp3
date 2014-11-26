a93628846139cd121879fe3c64c28c0164c14378
Eric Nordlund <eric@epitet.net>
2014-11-26 13:46:34 +0100

    JS-comments added

--- uppgift2/js/own.js
+++ uppgift2/js/own.js
@@ -1,8 +1,8 @@
 function login()
 {
+	//Create a prompt to get the user name
 	var login_name = prompt("Enter login name");
 	if (login_name != null) 
 		{
-			document.getElementById("login_box").innerHTML = "User: " + login_name;
+			document.getElementById("login_box").innerHTML = "User: " + login_name; //If a name have been entered, send it to #loginbox
 		}
-}
