6c3c28a27f81f828e7157393054150c3471b176f
EricNordlund <eric@epitet.net>
2014-12-02 14:28:05 +0100

    Added cookies to loginscript, implemented on index.html

--- uppgift2/js/own.js
+++ uppgift2/js/own.js
@@ -3,13 +3,32 @@ function login()
 	//Create a prompt to get the user name
 	var login_name = prompt("Enter login name");
 	if (login_name.length > 0) 
-		{
-			document.getElementById("login_box").innerHTML = "Logged in User: " + login_name; //If a name have been entered, send it to #loginbox
-		}
+	{
+		document.cookie = "userName=" + login_name + "; path=/";
+		checkLogin();
+	}
 }
 
+function checkLogin()
+{
+	if (getCookie("userName"))
+	document.getElementById("login_box").innerHTML = "Logged in User: " + getCookie("userName"); 
+}
+
+function getCookie(name)
+{
+	var cookies = "; " + document.cookie;
+	var values = cookies.split("; " + name + "="); //Split the array in two right before the attr. value.
+	if(values.length > 1) //Checks that the cookie can be found
+	return values.pop().split(";").shift(); //Return the second part of the array, split it to isolate the value, get the first value.
+	else
+	return false;
+}
+
+/*
 function formCheck()
 
 {
-	
+
 }
+*/
