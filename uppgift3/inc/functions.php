1b4b51ab8924a0b54da90fcd8964783a3de19b39
Samuelsson <erik.samuelsson@gmail.com>
2014-12-08 15:03:38 +0100

    first commit - file structure

---
+++ uppgift3/inc/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ *	functions.php
+ *
+ *	here we save all the php functions.
+ */
+
+/* ==|== Includes ===============================================================================
+	All files that should be included on the homepage, for example Header and Footer.
+   ============================================================================================== */
+
+	
+	//	Header
+	function get_header() {
+		include('header.php');
+	}
+
+	//	Footer
+	function get_footer() {
+		include('footer.php');
+	}
+
+?>
