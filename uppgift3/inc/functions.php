455f6e4eedc6e3e09991265df24b46d49f9a006e
Samuelsson <erik.samuelsson@gmail.com>
2014-12-09 01:20:41 +0100

    tweaking db connection

--- uppgift3/inc/functions.php
+++ uppgift3/inc/functions.php
@@ -28,16 +28,16 @@
 	// The function for the database connection and quering/execute. 
 	function db_handle() {
 		try {
-
 			$DBH = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS); // The PDO object for db connection.
 			$DBH->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, 1); // Attributes added to the PDO object.
-			$DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // Error attributes, if there is an error. Debugging etc.
+			$DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Error attributes, if there is an error. Debugging etc.
 			return $DBH;
 		}
 		catch (PDOException $e) {
-			print "Error!: " . $e->getMessage() . "<br>"; // Catches and shows an error msg if there is any.
+			print "There is a problem with the DB operation!: <br>" . $e->getMessage() . "<br>"; // Catches and shows an error msg if there is any.
 			die();
 		}
+
 	}
 
 
