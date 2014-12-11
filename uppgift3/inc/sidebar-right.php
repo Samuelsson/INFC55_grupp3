925b3a86237ada3bfb82fef126283bb2d34efd2c
Samuelsson <erik.samuelsson@gmail.com>
2014-12-11 21:02:39 +0100

    minor code improvements

--- uppgift3/inc/sidebar-right.php
+++ uppgift3/inc/sidebar-right.php
@@ -1,6 +1,9 @@
 	<div class="col-sm-4 col-md-3 sidebar-right">
 		<section>
-			<h4>Senaste Matcher</h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<h4>Kommande matcher</h4>
+			<p>Bacon ipsum dolor amet short ribs t-bone jerky shankle. Ham hock swine cow, tail ball tip kielbasa corned beef tenderloin fatback beef ribs meatball pig pork tri-tip flank. Salami porchetta flank pastrami. Shankle bacon chicken, ribeye hamburger tongue ground round meatloaf sirloin biltong jerky short ribs ham hock venison turducken. Ball tip bresaola pork prosciutto ham tenderloin. Pig short ribs doner swine.</p>
+
+			<h4>Senaste matcher</h4>
+			<p>Shank drumstick brisket strip steak ball tip spare ribs. Shank biltong bacon cupim ball tip. Porchetta hamburger fatback, t-bone pig cow turducken andouille ground round tri-tip sirloin strip steak corned beef. Jowl shoulder pig spare ribs pork chop ball tip. Beef ribs shankle fatback turkey, tail tri-tip ribeye pastrami kevin. Cupim hamburger shoulder andouille, chuck kevin alcatra. Prosciutto capicola salami, shank chicken bresaola bacon porchetta hamburger shoulder jerky.</p>
 		</section>
 	</div>
--- uppgift3/index.php
+++ uppgift3/index.php
@@ -9,210 +9,218 @@
 	<div class="container-fluid">
 		<div class="row">
 
-		<div class="col-sm-8 col-md-9 main-content">
+			<div class="col-sm-8 col-md-9 main-content">
 
-			<h1>Test Page</h1>
+				<h1>Testsida f&ouml;r Grupp 3</h1>
 
-			<div>
-				<h2>Division 1</h2>
-				<div class="table-responsive">
-					<table class="table table-striped">
-						<thead>
-							<tr>
-								<th>Placering</th>
-								<th>Plan</th>
-								<th>Lag</th>
-								<th>Resultat</th>
-							</tr>
-						</thead>
+				<div>
+					<h2>Division 1</h2>
+					<div class="table-responsive">
+						<table class="table table-striped">
+							
+							<thead>
+								<tr>
+									<th>Placering</th>
+									<th>Plan</th>
+									<th>Lag</th>
+									<th>Resultat</th>
+								</tr>
+							</thead>
 
-						<tbody>
-							<tr>
-								<td>1</td>
-								<td>3</td>
-								<td>AIK - HBK</td>
-								<td>2-5</td>
-							</tr>
-							<tr>
-								<td>2</td>
-								<td>2</td>
-								<td>HIF - Bajslaget</td>
-								<td>1-34</td>
-							</tr>
-							<tr>
-								<td>3</td>
-								<td>4</td>
-								<td>DIF - Coola Laget</td>
-								<td>2-2</td>
-							</tr>
-							<tr>
-								<td>4</td>
-								<td>1</td>
-								<td>Snygga Laget - Fula Laget</td>
-								<td>4-5</td>
-							</tr>
-							<tr>
-								<td>5</td>
-								<td>2</td>
-								<td>The Losers - The Winners</td>
-								<td>2-0</td>
-							</tr>
-						</tbody>
-					</table>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>3</td>
+									<td>AIK - HBK</td>
+									<td>2-5</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>2</td>
+									<td>HIF - Bajslaget</td>
+									<td>1-34</td>
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>4</td>
+									<td>DIF - Coola Laget</td>
+									<td>2-2</td>
+								</tr>
+								<tr>
+									<td>4</td>
+									<td>1</td>
+									<td>Snygga Laget - Fula Laget</td>
+									<td>4-5</td>
+								</tr>
+								<tr>
+									<td>5</td>
+									<td>2</td>
+									<td>The Losers - The Winners</td>
+									<td>2-0</td>
+								</tr>
+							</tbody>
+
+						</table>
+					</div>
 				</div>
-			</div>
 
-			<div>
-				<h2>Division 2</h2>
-				<div class="table-responsive">
-					<table class="table table-striped">
-						<thead>
-							<tr>
-								<th>Placering</th>
-								<th>Plan</th>
-								<th>Lag</th>
-								<th>Resultat</th>
-							</tr>
-						</thead>
+				<div>
+					<h2>Division 2</h2>
+					<div class="table-responsive">
+						<table class="table table-striped">
+							
+							<thead>
+								<tr>
+									<th>Placering</th>
+									<th>Plan</th>
+									<th>Lag</th>
+									<th>Resultat</th>
+								</tr>
+							</thead>
 
-						<tbody>
-							<tr>
-								<td>1</td>
-								<td>3</td>
-								<td>AIK - HBK</td>
-								<td>2-5</td>
-							</tr>
-							<tr>
-								<td>2</td>
-								<td>2</td>
-								<td>HIF - Bajslaget</td>
-								<td>1-34</td>
-							</tr>
-							<tr>
-								<td>3</td>
-								<td>4</td>
-								<td>DIF - Coola Laget</td>
-								<td>2-2</td>
-							</tr>
-							<tr>
-								<td>4</td>
-								<td>1</td>
-								<td>Snygga Laget - Fula Laget</td>
-								<td>4-5</td>
-							</tr>
-							<tr>
-								<td>5</td>
-								<td>2</td>
-								<td>The Losers - The Winners</td>
-								<td>2-0</td>
-							</tr>
-						</tbody>
-					</table>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>3</td>
+									<td>AIK - HBK</td>
+									<td>2-5</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>2</td>
+									<td>HIF - Bajslaget</td>
+									<td>1-34</td>
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>4</td>
+									<td>DIF - Coola Laget</td>
+									<td>2-2</td>
+								</tr>
+								<tr>
+									<td>4</td>
+									<td>1</td>
+									<td>Snygga Laget - Fula Laget</td>
+									<td>4-5</td>
+								</tr>
+								<tr>
+									<td>5</td>
+									<td>2</td>
+									<td>The Losers - The Winners</td>
+									<td>2-0</td>
+								</tr>
+							</tbody>
+
+						</table>
+					</div>
 				</div>
-			</div>
 
-			<div>
-				<h2>Division 3</h2>
-				<div class="table-responsive">
-					<table class="table table-striped">
-						<thead>
-							<tr>
-								<th>Placering</th>
-								<th>Plan</th>
-								<th>Lag</th>
-								<th>Resultat</th>
-							</tr>
-						</thead>
+				<div>
+					<h2>Division 3</h2>
+					<div class="table-responsive">
+						<table class="table table-striped">
+							
+							<thead>
+								<tr>
+									<th>Placering</th>
+									<th>Plan</th>
+									<th>Lag</th>
+									<th>Resultat</th>
+								</tr>
+							</thead>
 
-						<tbody>
-							<tr>
-								<td>1</td>
-								<td>3</td>
-								<td>AIK - HBK</td>
-								<td>2-5</td>
-							</tr>
-							<tr>
-								<td>2</td>
-								<td>2</td>
-								<td>HIF - Bajslaget</td>
-								<td>1-34</td>
-							</tr>
-							<tr>
-								<td>3</td>
-								<td>4</td>
-								<td>DIF - Coola Laget</td>
-								<td>2-2</td>
-							</tr>
-							<tr>
-								<td>4</td>
-								<td>1</td>
-								<td>Snygga Laget - Fula Laget</td>
-								<td>4-5</td>
-							</tr>
-							<tr>
-								<td>5</td>
-								<td>2</td>
-								<td>The Losers - The Winners</td>
-								<td>2-0</td>
-							</tr>
-						</tbody>
-					</table>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>3</td>
+									<td>AIK - HBK</td>
+									<td>2-5</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>2</td>
+									<td>HIF - Bajslaget</td>
+									<td>1-34</td>
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>4</td>
+									<td>DIF - Coola Laget</td>
+									<td>2-2</td>
+								</tr>
+								<tr>
+									<td>4</td>
+									<td>1</td>
+									<td>Snygga Laget - Fula Laget</td>
+									<td>4-5</td>
+								</tr>
+								<tr>
+									<td>5</td>
+									<td>2</td>
+									<td>The Losers - The Winners</td>
+									<td>2-0</td>
+								</tr>
+							</tbody>
+
+						</table>
+					</div>
 				</div>
-			</div>
 
-			<div>
-				<h2>Division 4</h2>
-				<div class="table-responsive">
-					<table class="table table-striped">
-						<thead>
-							<tr>
-								<th>Placering</th>
-								<th>Plan</th>
-								<th>Lag</th>
-								<th>Resultat</th>
-							</tr>
-						</thead>
+				<div>
+					<h2>Division 4</h2>
+					<div class="table-responsive">
+						<table class="table table-striped">
+							
+							<thead>
+								<tr>
+									<th>Placering</th>
+									<th>Plan</th>
+									<th>Lag</th>
+									<th>Resultat</th>
+								</tr>
+							</thead>
 
-						<tbody>
-							<tr>
-								<td>1</td>
-								<td>3</td>
-								<td>AIK - HBK</td>
-								<td>2-5</td>
-							</tr>
-							<tr>
-								<td>2</td>
-								<td>2</td>
-								<td>HIF - Bajslaget</td>
-								<td>1-34</td>
-							</tr>
-							<tr>
-								<td>3</td>
-								<td>4</td>
-								<td>DIF - Coola Laget</td>
-								<td>2-2</td>
-							</tr>
-							<tr>
-								<td>4</td>
-								<td>1</td>
-								<td>Snygga Laget - Fula Laget</td>
-								<td>4-5</td>
-							</tr>
-							<tr>
-								<td>5</td>
-								<td>2</td>
-								<td>The Losers - The Winners</td>
-								<td>2-0</td>
-							</tr>
-						</tbody>
-					</table>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>3</td>
+									<td>AIK - HBK</td>
+									<td>2-5</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>2</td>
+									<td>HIF - Bajslaget</td>
+									<td>1-34</td>
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>4</td>
+									<td>DIF - Coola Laget</td>
+									<td>2-2</td>
+								</tr>
+								<tr>
+									<td>4</td>
+									<td>1</td>
+									<td>Snygga Laget - Fula Laget</td>
+									<td>4-5</td>
+								</tr>
+								<tr>
+									<td>5</td>
+									<td>2</td>
+									<td>The Losers - The Winners</td>
+									<td>2-0</td>
+								</tr>
+							</tbody>
+
+						</table>
+					</div>
 				</div>
-			</div>
 
-		</div>
+			</div>
 
 		<?php
-			get_sidebarright(); // Loads the footer after the main content
+			get_sidebarright(); // Gets the sidebar and loads it after the main content
 		?>
 
 		</div>
