7f44ece69ad8a0e2c95033fdfea9620e4bdfd013
EricNordlund <eric@epitet.net>
2015-01-13 01:04:40 +0100

    Fixed merge bug

--- uppgift3/dal/TeamDal.php
+++ uppgift3/dal/TeamDal.php
@@ -12,6 +12,25 @@ class TeamDal {
 		$sqlQuery = "SELECT * FROM Teams";
 		return $this->dbh->query($sqlQuery);
 	}
+
+	function getTeamsForCup($cupId) {
+			$query = $this->dbh->query(
+				"SELECT * FROM Teams WHERE teamId IN ( 
+					SELECT teamId FROM DivisionTeam WHERE divisionId IN ( 
+						SELECT divisionId FROM Divisions WHERE cupId = '$cupId' 
+						) 
+					)");
+			$query->setFetchMode(\PDO::FETCH_CLASS, 'Team');
+
+			return $query->fetchAll();
+		}
+
+		function getTeamsForDivision($divisionId) {
+			$query = $this->dbh->query("SELECT * FROM Teams WHERE teamId IN 
+				(SELECT teamId FROM DivisionTeam WHERE divisionId = '$divisionId')");
+			$query->setFetchMode(\PDO::FETCH_CLASS, 'Team');
+			return $query->fetchAll();
+		}
 }
 
 ?>
