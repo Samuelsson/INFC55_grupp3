10161cca45609b42318e054e6a82e094ef57382d
Samuelsson <erik.samuelsson@gmail.com>
2015-01-16 05:33:44 +0100

    minor admin changes

--- uppgift3/view/admin.php
+++ uppgift3/view/admin.php
@@ -4,6 +4,11 @@
 	$controller = new Controller; // Creates a controller object
 	$controller -> getHeader(); // Loads the header before the main content
 
+	if(isset($_POST['email'])) {
+		$password = md5($_POST['password'] . SALT);
+		$values = $arrayName = array('email' => $_POST['email'], 'password' => $password, 'accessLevel' => $_POST['accessLevel']);
+		$controller->create("Users", $values);
+	}
 ?>
 
 	<!-- The Main Content
@@ -13,67 +18,52 @@
 		<div class="row">
 
 			<div class="col-md-12 admin-area">
+				<?php if(isset($_POST['email']))
+					echo "<p>Användaren är sparad</p>";
+				?>
 
 				<h1>Administration</h1>
 
-				<h2>Skapa anv&auml;ndare</h2>
+				<!-- Create User -->
 
-				<!-- Create Team Leader -->
-
-				<div class="panel panel-default">
+				<div class="panel panel-default admin-create-user">
 					<div class="panel-heading">
-						Skapa spelledare
+						Skapa anv&auml;ndare
 					</div>
 
 					<div class="panel-body">
-						<form>
-							<div class="form-group">
-								<label for="teamLeaderName">Namn</label>
-								<input type="text" class="form-control" id="teamLeaderName" placeholder="Spelledarens namn" required>
-							</div>
 
-							<div class="form-group">
-								<label for="teamLeaderPassword">L&ouml;senord</label>
-								<input type="password" class="form-control" id="teamLeaderPassword" placeholder="L&ouml;senord" required>
-							</div>
+						<div class="admin-form">
+							<form method="POST">
+								<div class="form-group">
+									<label for="email">E-mail</label>
+									<input type="text" class="form-control" id="email" name='email' placeholder="Användarens epost/inloggning" required>
+								</div>
 
-							<div class="form-group">
-								<input type="password" class="form-control" id="teamLeaderPasswordRepeat" placeholder="Upprepa l&ouml;senord" required>
-							</div>
+								<div class="form-group">
+									<label for="teamLeaderPassword">L&ouml;senord</label>
+									<input type="password" class="form-control" id="password" name="password" placeholder="L&ouml;senord" required>
+								</div>
 
-							<button type="submit" class="btn btn-default">Skapa</button>
-						</form>
-					</div>
-				</div>
+								<div class="form-group">
+									<input type="password" class="form-control" id="passwordRepeat" name="passwordRepeat" placeholder="Upprepa l&ouml;senord" required>
+								</div>
 
-				<hr>
+								<div class="form-group">
+									<label for="accessLevel">Klass</label>
+									<select class="form-control" id="accessLevel" name="accessLevel">
+									<!-- Should be gotten from the DB -->
+									<option value="9">Admin</option>
+									<option value="7">Cupmaster</option>
+									<option value="5">Fieldhost</option>
+									<option value="3">Teamowner</option>
+									<option value="1">Player</option>
+									</select>
+								</div>
 
-				<!-- Create Field Host -->
-
-				<div class="panel panel-default">
-
-					<div class="panel-heading">
-						Skapa planv&auml;rd
-					</div>
-
-					<div class="panel-body">
-						<form>
-							<div class="form-group">
-								<label for="fieldHostName">Namn</label>
-								<input type="text" class="form-control" id="fieldHostName" placeholder="Planv&auml;rdens namn" required>
-							</div>
-
-							<div class="form-group">
-								<label for="teamLeaderPassword">L&ouml;senord</label>
-								<input type="password" class="form-control" id="fieldHostPassword" placeholder="L&ouml;senord" required>
-							</div>
-
-							<div class="form-group">
-								<input type="password" class="form-control" id="fieldHostPasswordRepeat" placeholder="Upprepa l&ouml;senord" required>
-							</div>
-
-							<button type="submit" class="btn btn-default">Skapa</button>
-						</form>
+								<button type="submit" class="btn btn-default">Skapa</button>
+							</form>
+						</div>
 					</div>
 				</div>
 
