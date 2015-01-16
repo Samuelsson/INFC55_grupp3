bd1f6f43317a84852aecc52c5d9ecda5784c7e09
Samuelsson <erik.samuelsson@gmail.com>
2015-01-16 04:55:29 +0100

    refined logout

--- uppgift3/view/manage_cup.php
+++ uppgift3/view/manage_cup.php
@@ -3,6 +3,7 @@
 	require_once('../controller/controller.php'); // The file with all functions is required (can't be loaded more than once)
 	
 	$controller = new Controller; // Creates a controller object
+	$controller->checkLoggedInCookie();
 	$controller->setAccessLevel(7);
 
 	$controller -> getHeader(); // Loads the header before the main content
